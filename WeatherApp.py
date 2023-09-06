8700e36781b3eb928a7259ea3de01d6e7699b299
cheeseformoi <cheeseformoi@gmail.com>
2023-09-05 20:18:35 -0500

    made starter files

---
+++ WeatherApp.py
@@ -0,0 +1,6 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtCore import Qt
+
+class MainWindow():
+	
---
+++ main.py
@@ -0,0 +1,12 @@
+from WeatherApp import MainWindow
+from PyQt5 import QtWidgets
+import sys
+
+
+
+app = QtWidgets.QApplication(sys.argv)
+
+window = 
+window.show()
+app.exec()
+
